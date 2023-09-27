68305f01ba6d696ad730b724d45c4acd94376b66
Juanma0186 <juanma0186@gmail.com>
2023-09-27 21:18:01 +0200

    Añadido el contenido del HTML

---
+++ php/index.php
@@ -0,0 +1,48 @@
+<?php
+
+include('php/data.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Prueba Pull Request</title>
+</head>
+
+<body>
+  <div class="titulo">
+    <h1>GRUPO MDMJ</h1>
+    <h2>Integrantes</h2>
+    <ul>
+      <!--PHP-->
+    </ul>
+  </div>
+  <div class="contenido">
+    <h2>HORARIO</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>HORA</th>
+          <th>LUNES</th>
+          <th>MARTES</th>
+          <th>MIERCOLES</th>
+          <th>JUEVES</th>
+          <th>VIERNES</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!--PHP-->
+      </tbody>
+    </table>
+    <div class="asignaturas">
+      <!--PHP-->
+    </div>
+  </div>
+</body>
+
+</html>
